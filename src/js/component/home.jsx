492f1436e5f062c02d19ae220bd9fa21d76b289d
fmatovelle <167099575+fmatovelle@users.noreply.github.com>
2024-07-25 10:43:50 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,127 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+// import Styles from './styles';
+// import Title from "./title";
+// import Todos from "./todos";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [newEntry, setNewEntry] = useState('');
+	const [taskList, setNewList] = useState([]);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				"label": newEntry,
+				"is_done": false
+			})
+		};
+		fetch("https://playground.4geeks.com/todo/todos/federico", requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				getTasks()
+				console.log(result)
+				setNewEntry("")
+			})
+			.catch((error) => console.error(error));
+	}
+	console.log(taskList);
+	const createUser = () => {
+		const requestOptions = {
+			method: "POST",
+			redirect: "follow"
+		};
+
+		fetch("https://playground.4geeks.com/todo/users/federico", requestOptions)
+			.then((response) => response.json())
+			.then((result) => {
+				getTasks()
+				console.log(result)
+			})
+			.catch((error) => console.error(error));
+	}
+
+	const getTasks = () => {
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+
+		fetch("https://playground.4geeks.com/todo/users/federico", requestOptions)
+			.then((response) => {
+				console.log(response);
+				if (response.ok === false) {
+					createUser()
+				} else { return response.json() }
+			})
+
+			.then((result) => setNewList(result.todos))
+			.catch((error) => console.error(error));
+	}
+	// getTasks()
+	const deleteTask = (id) => {
+		const requestOptions = {
+			method: "DELETE",
+			redirect: "follow"
+		};
+
+		fetch("https://playground.4geeks.com/todo/todos/" + id, requestOptions)
+			.then((response) => response.text())
+			.then((result) => {
+				getTasks()
+				console.log(result)
+			})
+			.catch((error) => console.error(error));
+
+	}
+	useEffect(() => {
+		getTasks()
+	}, [])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container w-50">
+			<h1>Todos</h1>
+			<form onSubmit={handleSubmit}>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail" className="from-label"></label>
+					<input
+						placeholder="What needs to be done?"
+						onChange={(event) => setNewEntry(event.target.value)}
+						onKeyDown={(event) => {
+							if (event.key === 'Enter') {
+								handleSubmit(event)
+							}
+						}}
+						value={newEntry}
+					/>
+				</div>
+
+			</form>
+			<ul className="todo">
+				{
+					taskList.map((task, index) => {
+						return (
+
+							<li key={index}>{task.label}{' '}
+								<i 
+									onClick={() => { deleteTask(task.id)}}
+									className="fa-solid fa-trash">
+								</i>
+							</li>
+							
+								
+							
+						)
+					})
+				}
+
+			</ul>
 		</div>
+
 	);
 };
 
